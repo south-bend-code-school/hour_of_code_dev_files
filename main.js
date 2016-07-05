f108a5b7187f8462acc7322999892956ec89f6d8
asejdina <asejdina@gmail.com>
2016-07-05 19:18:05 -0400

    made it so that colors of buttons autoset

--- main.js
+++ main.js
@@ -3,14 +3,22 @@
   $(document).ready(initialize);
 
   function initialize(){
+    setBGColor();
     $('button').click(changeColor);
   }
-
 
   function changeColor(){
     var newColor = $(this).text();
     console.log(newColor);
     $('h2').css('color', newColor);
+  }
+
+  function setBGColor(){
+    $('button').each(function(i){
+      var bgColor = $(this).text();
+      console.log(bgColor);
+      $(this).css('background-color', bgColor);
+    });
   }
 
 })();
